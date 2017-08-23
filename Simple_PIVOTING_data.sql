ba022d3666f392bc05e4feefe94c457802fe3e78
bongho <30528502+bongho@users.noreply.github.com>
2017-08-23 11:02:46 +0100

    Create Simple_PIVOTING_data.sql

---
+++ Simple_PIVOTING_data.sql
@@ -0,0 +1,28 @@
+or this challenge you need to PIVOT data. You have two tables, products and details. Your task is to pivot the rows in products to produce a table of products which have rows of their detail. Group and Order by the name of the Product.
+
+Tables and relationship below:
+
+
+
+You must use the CROSSTAB statement to create a table that has the schema as below:
+
+CROSSTAB table.
+
+name
+good
+ok
+bad
+Compare your table to the expected table to view the expected results.
+
+CREATE EXTENSION tablefunc;
+-- Create your CROSSTAB statement here
+
+SELECT * 
+FROM CROSSTAB(
+              'SELECT A.NAME,B.DETAIL,COUNT(B.*) ct
+               FROM PRODUCTS A, DETAILS B
+               WHERE A.ID = B.PRODUCT_ID
+               GROUP BY 1,2
+               ORDER BY 1,2'
+               ,$$VALUES ('good'::text), ('ok'::text),('bad'::text)$$)
+AS ct (NAME text, good int, ok int, bad int); 
